dd20f2cca779182d96b983894f852e49c45d23f7
Pavel Belevich <belevichp@gmail.com>
2017-03-02 14:25:04 -0500

    Sliding Window Maximum

---
+++ src/main/java/com/leetcode/problems/slidingWindowMaximum/Solution.java
@@ -0,0 +1,31 @@
+package com.leetcode.problems.slidingWindowMaximum;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author Pavel Belevich
+ */
+public class Solution {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> deque = new LinkedList<>();
+        int[] maxNumbers = new int[nums.length - k + 1];
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (!deque.isEmpty() && nums[i] >= nums[deque.getLast()]) {
+                deque.removeLast(); //Drop all prev less then current
+            }
+            deque.addLast(i); //Add current to the end
+            if (i >= k && !deque.isEmpty() && deque.getFirst() == i - k) {
+                deque.removeFirst(); //Drop max if it's on beyond the k
+            }
+            if (i >= k - 1) {
+                maxNumbers[j++] = nums[deque.getFirst()];
+            }
+        }
+
+        return maxNumbers;
+    }
+
+}
---
+++ src/test/java/com/leetcode/problems/slidingWindowMaximum/SolutionTest.java
@@ -0,0 +1,18 @@
+package com.leetcode.problems.slidingWindowMaximum;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Pavel Belevich
+ */
+public class SolutionTest {
+
+    @Test
+    public void testMaxSlidingWindow() throws Exception {
+        assertArrayEquals(new int[]{3, 3, 5, 5, 6, 7}, new Solution().maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3));
+        assertArrayEquals(new int[]{10, 9, 8, 7, 6, 5, 4, 3}, new Solution().maxSlidingWindow(new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}, 3));
+    }
+
+}
