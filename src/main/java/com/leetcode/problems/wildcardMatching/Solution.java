b58033d13bf33085c72dab53949a4170ab28b3a6
Pavel Belevich <belevichp@gmail.com>
2017-03-10 15:19:04 -0500

    44. Wildcard Matching - dp

--- src/main/java/com/leetcode/problems/wildcardMatching/Solution.java
+++ src/main/java/com/leetcode/problems/wildcardMatching/Solution.java
@@ -1,20 +1,18 @@
 package com.leetcode.problems.wildcardMatching;
 
-import java.util.Arrays;
-
 /**
  * https://leetcode.com/problems/wildcard-matching/
- *
+ * <p>
  * 44. Wildcard Matching
- *
+ * <p>
  * '?' Matches any single character.
  * '*' Matches any sequence of characters (including the empty sequence).
- *
+ * <p>
  * The matching should cover the entire input string (not partial).
- *
+ * <p>
  * The function prototype should be:
  * bool isMatch(const char *s, const char *p)
- *
+ * <p>
  * Some examples:
  * isMatch("aa","a") → false
  * isMatch("aa","aa") → true
@@ -29,30 +27,36 @@ import java.util.Arrays;
 public class Solution {
 
     public boolean isMatch(String s, String p) {
-        final char[] sChars = Arrays.copyOf(s.toCharArray(), s.length() + 1);
-        final char[] pChars = Arrays.copyOf(p.toCharArray(), p.length() + 1);
-        return isMatch(sChars, 0, pChars, 0);
+        final char[] sChars = s.toCharArray();
+        final char[] pChars = p.toCharArray();
+
+        final boolean[][] isMatch = new boolean[sChars.length + 2][pChars.length + 2];
+
+        isMatch[sChars.length + 1][pChars.length + 1] = true;
+
+        for (int j = pChars.length; j >= 0; j--) {
+            for (int i = sChars.length; i >= 0; i--) {
+                final char sChar = charAt(sChars, i);
+                final char pChar = charAt(pChars, j);
+                if (sChar == pChar || pChar == '?') {
+                    isMatch[i][j] = isMatch[i + 1][j + 1];
+                } else if (pChar == '*') {
+                    boolean result = false;
+                    for (int k = i; k < sChars.length + 2 && !result; k++) {
+                        result = isMatch[k][j + 1];
+                    }
+                    isMatch[i][j] = result;
+                } else {
+                    isMatch[i][j] = false;
+                }
+            }
+        }
+
+        return isMatch[0][0];
     }
 
-    boolean isMatch(char[] sChars, int i, char[] pChars, int j) {
-        if (i == sChars.length && j == pChars.length) {
-            return true;
-        } else if (i == sChars.length || j == pChars.length) {
-            return false;
-        }
-        final char sChar = sChars[i];
-        final char pChar = pChars[j];
-        if (sChar == pChar || pChar == '?') {
-            return isMatch(sChars, i + 1, pChars, j + 1);
-        } else if (pChar == '*') {
-            boolean result = false;
-            for (int k = i; k <= sChars.length; k++) {
-                result |= isMatch(sChars, k, pChars, j + 1);
-            }
-            return result;
-        } else {
-            return false;
-        }
+    private char charAt(char[] s, int i) {
+        return i < s.length ? s[i] : 0;
     }
 
 }
