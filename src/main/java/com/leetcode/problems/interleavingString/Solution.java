b4eff8dd1b00b754d0372147c2040aa7dcf5211e
Pavel Belevich <belevichp@gmail.com>
2017-03-20 16:39:54 -0400

    97. Interleaving String - dp

--- src/main/java/com/leetcode/problems/interleavingString/Solution.java
+++ src/main/java/com/leetcode/problems/interleavingString/Solution.java
@@ -26,7 +26,25 @@ public class Solution {
         final char[] chars1 = s1.toCharArray();
         final char[] chars2 = s2.toCharArray();
         final char[] chars3 = s3.toCharArray();
-        return counting(chars1, chars2, chars3) && isInterleave(new Boolean[s1.length() + 1][s2.length() + 1][s3.length() + 1], chars1, 0, chars2, 0, chars3, 0);
+        if (!counting(chars1, chars2, chars3)) {
+            return false;
+        }
+        boolean[][] isInterleave = new boolean[chars1.length + 1][chars2.length + 1];
+        for (int i1 = chars1.length; i1 >= 0; i1--) {
+            for (int i2 = chars2.length; i2 >= 0; i2--) {
+                int i3 = i1 + i2;
+                if (i1 == chars1.length && i2 == chars2.length && i3 == chars3.length) {
+                    isInterleave[i1][i2] = true;
+                } else if (i1 < chars1.length && i2 < chars2.length && i3 < chars3.length && chars1[i1] == chars3[i3] && chars2[i2] == chars3[i3]) {
+                    isInterleave[i1][i2] = isInterleave[i1 + 1][i2] || isInterleave[i1][i2 + 1];
+                } else if (i1 < chars1.length && i3 < chars3.length && chars1[i1] == chars3[i3]) {
+                    isInterleave[i1][i2] = isInterleave[i1 + 1][i2];
+                } else if (i2 < chars2.length && i3 < chars3.length && chars2[i2] == chars3[i3]) {
+                    isInterleave[i1][i2] = isInterleave[i1][i2 + 1];
+                }
+            }
+        }
+        return isInterleave[0][0];
     }
 
     boolean counting(char[] s1, char[] s2, char[] s3) {
@@ -46,20 +64,6 @@ public class Solution {
             }
         }
         return true;
-    }
-
-    boolean isInterleave(Boolean[][][] cache, char[] s1, int i1, char[] s2, int i2, char[] s3, int i3) {
-        if (cache[i1][i2][i3] == null) {
-            if (i1 == s1.length && i2 == s2.length && i3 == s3.length) {
-                cache[i1][i2][i3] = true;
-            } else if (i1 < s1.length && i2 < s2.length && i3 < s3.length && s1[i1] == s3[i3] && s2[i2] == s3[i3]) {
-                cache[i1][i2][i3] = isInterleave(cache, s1, i1 + 1, s2, i2, s3, i3 + 1) || isInterleave(cache, s1, i1, s2, i2 + 1, s3, i3 + 1);
-            } else if (i1 < s1.length && i3 < s3.length && s1[i1] == s3[i3]) {
-                cache[i1][i2][i3] = isInterleave(cache, s1, i1 + 1, s2, i2, s3, i3 + 1);
-            } else
-                cache[i1][i2][i3] = i2 < s2.length && i3 < s3.length && s2[i2] == s3[i3] && isInterleave(cache, s1, i1, s2, i2 + 1, s3, i3 + 1);
-        }
-        return cache[i1][i2][i3];
     }
 
 }
