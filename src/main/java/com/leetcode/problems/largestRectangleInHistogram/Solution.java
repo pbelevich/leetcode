1ca2a0adaab10ff6ce30381d5b2538806520944c
Pavel Belevich <belevichp@gmail.com>
2017-04-14 22:35:18 -0400

    84. Largest Rectangle in Histogram - accepted

--- src/main/java/com/leetcode/problems/largestRectangleInHistogram/Solution.java
+++ src/main/java/com/leetcode/problems/largestRectangleInHistogram/Solution.java
@@ -1,16 +1,19 @@
 package com.leetcode.problems.largestRectangleInHistogram;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * https://leetcode.com/problems/largest-rectangle-in-histogram/#/description
- *
+ * <p>
  * 84. Largest Rectangle in Histogram
- *
+ * <p>
  * Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
- *
+ * <p>
  * Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
- *
+ * <p>
  * The largest rectangle is shown in the shaded area, which has area = 10 unit.
- *
+ * <p>
  * For example,
  * Given heights = [2,1,5,6,2,3],
  * return 10.
@@ -19,31 +22,25 @@ package com.leetcode.problems.largestRectangleInHistogram;
  */
 public class Solution {
 
-    private int[] heights;
-
     public int largestRectangleArea(int[] heights) {
-        this.heights = heights;
-        return largestRectangleArea(0, heights.length - 1);
-    }
-
-    private int largestRectangleArea(int lo, int hi) {
-        final int length = hi - lo + 1;
-        if (length == 0) {
-            return 0;
-        }
-        final int minIndex = minIndex(lo, hi);
-        return Math.max(length * heights[minIndex],
-                Math.max(largestRectangleArea(lo, minIndex - 1), largestRectangleArea(minIndex + 1, hi)));
-    }
-
-    private int minIndex(int lo, int hi) {
-        int minIndex = lo;
-        for (int i = lo + 1; i <= hi; i++) {
-            if (heights[i] < heights[minIndex]) {
-                minIndex = i;
+        Deque<Integer> stack = new ArrayDeque<>(heights.length);
+        int maxArea = 0;
+        int i;
+        for (i = 0; i < heights.length;) {
+            if (stack.isEmpty() || heights[i] >= heights[stack.peek()]) {
+                stack.push(i++);
+            } else {
+                while (!stack.isEmpty() && heights[i] < heights[stack.peek()]) {
+                    int tp = stack.pop();
+                    maxArea = Math.max(maxArea, heights[tp] * (stack.isEmpty() ? i : i - stack.peek() - 1));
+                }
             }
         }
-        return minIndex;
+        while (!stack.isEmpty()) {
+            int tp = stack.pop();
+            maxArea = Math.max(maxArea, heights[tp] * (stack.isEmpty() ? i : i - stack.peek() - 1));
+        }
+        return maxArea;
     }
 
 }
