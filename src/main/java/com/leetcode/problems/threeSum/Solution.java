6e89d1806864be4f68490a10ce474fb2433471ae
Pavel Belevich <belevichp@gmail.com>
2017-04-19 00:31:40 -0400

    15. 3Sum

--- src/main/java/com/leetcode/problems/threeSum/Solution.java
+++ src/main/java/com/leetcode/problems/threeSum/Solution.java
@@ -2,55 +2,77 @@ package com.leetcode.problems.threeSum;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
+/**
+ * https://leetcode.com/problems/3sum/#/description
+ * <p>
+ * 15. 3Sum
+ * <p>
+ * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+ * <p>
+ * Note: The solution set must not contain duplicate triplets.
+ * <p>
+ * For example, given array S = [-1, 0, 1, 2, -1, -4],
+ * <p>
+ * A solution set is:
+ * [
+ * [-1, 0, 1],
+ * [-1, -1, 2]
+ * ]
+ */
 public class Solution {
 
     public List<List<Integer>> threeSum(int[] nums) {
-        if (nums.length < 3) {
-            return Collections.emptyList();
-        }
+        List<List<Integer>> result = new ArrayList<>();
+
+//        Arrays.sort(nums);
 
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
         for (int n : nums) {
-            if (n < min) {
-                min = n;
-            }
-            if (n > max) {
-                max = n;
-            }
+            min = Math.min(min, n);
+            max = Math.max(max, n);
         }
 
         int[] map = new int[max - min + 1];
-        for (int i = 0; i < nums.length; i++) {
-            map[nums[i] - min] = i;
-        }
-
-        List<List<Integer>> result = new ArrayList<>();
-
-        boolean zeros = false;
-        for (int i = 0; i < nums.length; i++) {
-            int a = nums[i];
-            for (int j = i + 1; j < nums.length; j++) {
-                int b = nums[j];
-                if (b >= a) {
-                    int c = -a - b;
-                    if (c >= min && c <= max && map[c - min] != 0) {
-                        if (c >= b) {
-                            if (a == 0 && b == 0 && c == 0) {
-                                zeros = true;
-                            } else {
-                                result.add(Arrays.asList(a, b, c));
-                            }
-                        }
-                    }
-                }
+        int distinct = 0;
+        for (int n : nums) {
+            if (map[n - min]-- == 0) {
+                distinct++;
             }
         }
-        if (zeros) {
-            result.add(Arrays.asList(0, 0, 0));
+
+        int[] idx = new int[max - min + 1];
+        int[] nums2 = new int[distinct];
+        int p = 0;
+        for (int n : nums) {
+            if (map[n - min] < 0) {
+                nums2[p] = n;
+                idx[n - min] = p;
+                p++;
+                map[n - min] = -map[n - min];
+            }
+        }
+
+        for (int i = 0; i < nums2.length; i++) {
+            int a = nums2[i];
+            map[a - min]--;
+            for (int j = i + (map[a - min] == 0 ? 1 : 0); j < nums2.length; j++) {
+                int b = nums2[j];
+                map[b - min]--;
+
+                int c = 0 - a - b;
+                if (0 <= c - min && c - min < map.length && map[c - min] != 0) {
+                    int k = idx[c - min];
+                    if (k >= j) {
+                        result.add(Arrays.asList(a, b, c));
+                    }
+                }
+
+                map[b - min]++;
+            }
+            map[a - min]++;
         }
 
         return result;
--- src/test/java/com/leetcode/problems/threeSum/SolutionTest.java
+++ src/test/java/com/leetcode/problems/threeSum/SolutionTest.java
@@ -1,13 +1,8 @@
 package com.leetcode.problems.threeSum;
 
-import org.hamcrest.Matcher;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
-
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
 
 public class SolutionTest {
 
@@ -15,9 +10,7 @@ public class SolutionTest {
 
     @Test
     public void testThreeSum() throws Exception {
-//        Matcher<Iterable<?>> iterableMatcher = iterableWithSize(1);
-//        Matcher<Iterable<? extends List<Integer>>> contains = contains(Arrays.asList(0, 0, 0));
-//        assertThat(SOLUTION.threeSum(new int[] {0, 0, 0}), allOf(iterableMatcher, contains));
+        final List<List<Integer>> actual = SOLUTION.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
     }
 
 }
