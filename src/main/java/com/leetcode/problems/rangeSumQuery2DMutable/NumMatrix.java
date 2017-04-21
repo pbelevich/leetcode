2bb43d7493a897ad5d739a19e6c816d763954d3a
Pavel Belevich <belevichp@gmail.com>
2017-04-21 00:37:33 -0400

    308. Range Sum Query 2D - Mutable

--- src/main/java/com/leetcode/problems/rangeSumQuery2DMutable/NumMatrix.java
+++ src/main/java/com/leetcode/problems/rangeSumQuery2DMutable/NumMatrix.java
@@ -2,31 +2,62 @@ package com.leetcode.problems.rangeSumQuery2DMutable;
 
 /**
  * https://leetcode.com/problems/range-sum-query-2d-mutable/#/description
- *
+ * <p>
  * 308. Range Sum Query 2D - Mutable
  *
  * @author Pavel Belevich
  */
 public class NumMatrix {
 
+    private int[][] nums;
+    private int[][] tree;
+    private int m;
+    private int n;
+
     public NumMatrix(int[][] matrix) {
-        for (int r = 0; r < matrix.length; r++) {
-            for (int c = 0; c < matrix[r].length; c++) {
+        if (matrix.length == 0 || matrix[0].length == 0) return;
+        n = matrix.length;
+        m = matrix[0].length;
+        this.nums = new int[n][m];
+        this.tree = new int[n + 1][m + 1];
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
                 update(r, c, matrix[r][c]);
             }
         }
     }
 
     public void update(int row, int col, int val) {
+        if (m == 0 || n == 0) return;
+        int diff = val - nums[row][col];
+        nums[row][col] = val;
+        for (int r = row + 1; r <= n; r = next(r)) {
+            for (int c = col + 1; c <= m; c = next(c)) {
+                tree[r][c] += diff;
+            }
+        }
+    }
 
+    private int next(int index) {
+        return index + (index & -index);
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return sum(row2, col2) - sum(row1, col2) - sum(row2, col1) + sum(row1, col1);
+        return sum(row2 + 1, col2 + 1) - sum(row2 + 1, col1) - sum(row1, col2 + 1) + sum(row1, col1);
     }
 
-    private int sum(int r, int c) {
-        return 0;
+    private int sum(int row, int col) {
+        int sum = 0;
+        for (int r = row; r > 0; r = parent(r)) {
+            for (int c = col; c > 0; c = parent(c)) {
+                sum += tree[r][c];
+            }
+        }
+        return sum;
+    }
+
+    private int parent(int index) {
+        return index - (index & -index);
     }
 
 }
--- src/test/java/com/leetcode/problems/rangeSumQuery2DMutable/NumMatrixTest.java
+++ src/test/java/com/leetcode/problems/rangeSumQuery2DMutable/NumMatrixTest.java
@@ -1,6 +1,5 @@
 package com.leetcode.problems.rangeSumQuery2DMutable;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,9 +8,9 @@ import static org.junit.Assert.*;
  * @author Pavel Belevich
  */
 public class NumMatrixTest {
-    @Ignore
+
     @Test
-    public void sumRegion() throws Exception {
+    public void sumRegion1() throws Exception {
         final NumMatrix numMatrix = new NumMatrix(new int[][]{
                 {3, 0, 1, 4, 2},
                 {5, 6, 3, 2, 1},
@@ -22,6 +21,19 @@ public class NumMatrixTest {
         assertEquals(8, numMatrix.sumRegion(2, 1, 4, 3));
         numMatrix.update(3, 2, 2);
         assertEquals(10, numMatrix.sumRegion(2, 1, 4, 3));
+    }
+
+    @Test
+    public void sumRegion2() throws Exception {
+        final NumMatrix numMatrix = new NumMatrix(new int[][]{
+                {1, 2}
+        });
+        assertEquals(1, numMatrix.sumRegion(0, 0, 0, 0));
+        assertEquals(2, numMatrix.sumRegion(0, 1, 0, 1));
+        assertEquals(3, numMatrix.sumRegion(0, 0, 0, 1));
+        numMatrix.update(0, 0, 3);
+        numMatrix.update(0, 1, 5);
+        assertEquals(8, numMatrix.sumRegion(0, 0, 0, 1));
     }
 
 }
