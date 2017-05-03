ffebac0398451ed20962ee9642aaf4e738719c7a
Pavel Belevich <belevichp@gmail.com>
2017-05-02 22:21:25 -0400

    373. Find K Pairs with Smallest Sums

---
+++ src/main/java/com/leetcode/problems/findKPairsWithSmallestSums/Solution.java
@@ -0,0 +1,7 @@
+package com.leetcode.problems.findKPairsWithSmallestSums;
+
+/**
+ * @author Pavel Belevich
+ */
+public class Solution {
+}
---
+++ src/test/java/com/leetcode/problems/findKPairsWithSmallestSums/SolutionTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * @author Pavel Belevich
+ */
+public class SolutionTest {
+
+}
